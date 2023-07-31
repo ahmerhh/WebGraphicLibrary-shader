c1d11a6ba1cddbff8c5c13b46899abc731a80302
ahmerhabib <60505302+ahmerhh@users.noreply.github.com>
2023-07-31 21:58:47 +0100

    update readme + test/index

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # WebGraphicLibrary-shader
- 
+
+WebGL shader wrapper.
+
+## Installation
+
+```sh
+$ npm install --save @ahmerhh/WebGraphicLibrary-shader
+```
+
+## Usage
+
+```js
+import Shader from '@ahmerhh/WebGraphicLibrary-shader';
+
+// setup canvas and gl
+
+const vertexShader = new Shader(gl, gl.VERTEX_SHADER,
+  `
+  attribute vec3 aPos;
+
+  void main() {
+    gl_Position = vec4(aPos, 1.0);
+  }
+`);
+```
+
+## API
+
+#### `shader = new Shader(gl, type, src)`
+
+Create a new shader, where:
+- `gl` is the [WebGL context](https://github.com/ahmerhh/WebGraphicLibrary-context).
+- `type` is the shader type: `gl.VERTEX_SHADER` or `gl.FRAGMENT_SHADER`.
+- `src` is the shader source, as a `string`.
+
+#### `shader.dispose()`
+
+Delete instance. Calls `gl.deleteShader`.
+
+## License
+
+MIT, see [LICENSE.md](https://github.com/ahmerhh/WebGraphicLibrary-shader/blob/master/LICENSE.md) for more details.
+
+## Credits
+
+Thanks to the amazing [stackgl](http://stack.gl/) for the inspiration.
--- test/index.js
+++ test/index.js
@@ -1,75 +1,34 @@
-import test from 'tape';
-import getGl from '@ahmerhh/WebGraphicLibrary-context';
-import Shader from '../src';
-
-const canvas = document.createElement('canvas');
-const gl = getGl(canvas);
-
-const vertexSrc = `
-  attribute vec3 aPosition;
-
-  void main() {
-    gl_Position = vec4(aPosition, 1.0);
+/**
+ * @class Shader
+ */
+export default class Shader {
+    /**
+     * @constructs Shader
+     * @param {WebGLRenderingContext} gl
+     * @param {uint} type - gl.VERTEX_SHADER or gl.FRAGMENT_SHADER 
+     * @param {string} src
+     */
+    constructor(gl, type, src) {
+      this.gl = gl;
+  
+      this.shader = this.gl.createShader(type);
+  
+      this.gl.shaderSource(this.shader, src);
+      this.gl.compileShader(this.shader);
+  
+      if(!this.gl.getShaderParameter(this.shader, this.gl.COMPILE_STATUS)) {
+        const error = this.gl.getShaderInfoLog(this.shader);
+        throw new Error(`Error while compiling shader: ${error}`);
+      }
+    }
+  
+    /**
+     * @method dispose
+     * @public
+     */
+    dispose() {
+      this.gl.deleteShader(this.shader);
+      this.shader = null;
+    }
   }
-`;
-
-const fragmentSrc = `
-  precision mediump float;
-
-  void main() {
-    gl_FragColor = vec4(1.0);
-  }
-`;
-
-const invalidSrc = `
-  void main() {
-  }
-`;
-
-// Test: Shader should be instantiable
-test('should be instanciable', t => {
-  t.plan(1);
-
-  const shader = new Shader(gl, gl.VERTEX_SHADER, vertexSrc);
-
-  t.ok(shader instanceof Shader, 'instance of Shader');
-});
-
-// Test: Shader should accept vertex and fragment shaders
-test('should accept vertex and fragment shader', t => {
-  t.plan(1);
-
-  const vertexShader = new Shader(gl, gl.VERTEX_SHADER, vertexSrc);
-  const fragmentShader = new Shader(gl, gl.FRAGMENT_SHADER, fragmentSrc);
-
-  t.pass('success creating a vertex and fragment shader');
-});
-
-// Test: Shader should throw with invalid source
-test('should throws with invalid source', t => {
-  t.plan(1);
-
-  t.throws(() => new Shader(gl, gl.VERTEX_SHADER, invalidSrc), Error, 'throws with invalid source');
-});
-
-// Test: Shader should expose the WebGLShader
-test('should expose the WebGLShader', t => {
-  t.plan(1);
-
-  const shader = new Shader(gl, gl.VERTEX_SHADER, vertexSrc);
-
-  t.ok(shader.shader instanceof WebGLShader, 'attribute shader is instance of WebGLShader');
-});
-
-// Test: Shader should delete the WebGLShader when disposing
-test('should delete the WebGLShader when disposing', t => {
-  t.plan(1);
-
-  const shader = new Shader(gl, gl.VERTEX_SHADER, vertexSrc);
-  shader.dispose();
-
-  t.equal(shader.shader, null, 'shader deleted');
-});
-
-// Close the window when the tests are finished
-test.onFinish(window.close.bind(window));
+  
